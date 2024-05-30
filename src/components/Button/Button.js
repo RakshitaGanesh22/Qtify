5d39d37f4a27dc1aa6a49ac440195749d884d238
RakshitaGanesh22 <rakshitaganesh705@gmail.com>
2024-05-30 23:21:38 +0530

    button done

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,10 +2,8 @@ import {Button} from "@mui/material";
 import Styles from './Button.module.css'; 
 export default function SearchButton (props){
     return (
-        <div className={Styles.button1}>
-            <form >
-                <button type="submit" className={Styles.button2}>{props.name}</button>
-            </form>
-        </div>
+        
+            <button  className={Styles.btn}>{props.name}</button>
+        
     )
 }
