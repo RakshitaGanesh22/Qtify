0d0461e9d683add0f81e38e840df5237081a6ecd
RakshitaGanesh22 <rakshitaganesh705@gmail.com>
2024-05-31 00:18:07 +0530

    logo css changed-1234

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import Styles from './Logo.module.css';
 import untitled from './untitled.png';
 
 export default function Logo() {
     return (
-            <img src={untitled} alt="Logo" className={Styles.Logo}/>
+            <img src={untitled} alt="Logo" />
     );
 }
